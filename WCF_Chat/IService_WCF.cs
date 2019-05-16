3e354398bcdf6c4ac4a76755500e37a2ca94a775
Revolyram <revolyram@gmail.com>
2019-05-16 23:02:07 +0300

    add realisation WCF_Chat/IService_WCF.cs

---
+++ WCF_Chat/IService_WCF.cs
@@ -0,0 +1,32 @@
+﻿using System.ServiceModel;
+
+namespace WCF_Chat
+{
+    [ServiceContract(CallbackContract = typeof(ICallBack))]
+    public interface IService_WCF
+    {
+        /// <summary>
+        /// Метод подключения
+        /// </summary>
+        [OperationContract]
+        int Connect(string name);
+
+        /// <summary>
+        /// Метод отключения
+        /// </summary>
+        [OperationContract]
+        void DisConnect(int id);
+
+        /// <summary>
+        /// Метод отправки сообщения
+        /// </summary>
+        [OperationContract(IsOneWay = true)]
+        void Smsg(string msg, int id);
+    }
+
+    public interface ICallBack
+    {
+        [OperationContract(IsOneWay =true)]
+        void CallBackMSG(string msg);
+    }
+}
