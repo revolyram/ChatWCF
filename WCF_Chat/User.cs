c48d7eba9732e12f2be31697e321a0cc9649d6c7
Revolyram <revolyram@gmail.com>
2019-05-16 23:04:30 +0300

    add realisation WCF_Chat/User.cs

---
+++ WCF_Chat/User.cs
@@ -0,0 +1,13 @@
+﻿using System.ServiceModel;
+namespace WCF_Chat
+{
+    class User
+    {
+        // Имя пользователя
+        public string Name { get; set; }
+        // Уникальный номер
+        public int IdU { get; set; }
+        // Переменная операции
+        public OperationContext operationContext { get; set; }
+    }
+}
